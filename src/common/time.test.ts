392016380e37b07a9baaeadec65ad18577b44e30
Tatsuto Yamamoto <laminne33569@approvers.dev>
2023-02-23 18:04:38 +0900

    fix: tzの設定

--- src/common/time.test.ts
+++ src/common/time.test.ts
@@ -3,12 +3,14 @@ import { DateObjectToISODate, ISODateToObject } from "./time";
 
 describe("", () => {
   it("時刻を正しくパースできる", () => {
+    process.env.TZ = "UTC";
     expect(ISODateToObject("2023-02-23T00:00:00+09:00")).toStrictEqual(
       new Date(2023, 1, 23, 0, 0, 0, 0)
     );
   });
 
   it("時刻を正しく文字列にできる", () => {
+    process.env.TZ = "UTC";
     expect(
       DateObjectToISODate(new Date("2023-02-23T00:00:00+09:00"))
     ).toStrictEqual("2023-02-23T00:00:00+09:00");
